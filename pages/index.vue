74135da2975e9ffdbd3d34804b39e9faad422d51
Juan Vargas <juanmvg2006@gmail.com>
2021-07-23 11:15:41 -0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Welcome
+  </div>
+</template>
+
+<script>
+export default {
+  // mounted(){
+    // this.$auth.loginWith('laravelSanctum', {
+    //   data: {
+    //     email: '',
+    //     password: ''
+    //   }
+    // });
+  // }
+}
+</script>
